f4cd78b9b271038e3a77c1b1d4ad0e626b0390bd
raymond wang <waymondrang@users.noreply.github.com>
2022-01-14 03:16:51 -0800

    gtag

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -21,8 +21,19 @@ module.exports = {
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ["link", { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" }]
+    ["link", { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" }],
+    ["script", { async: true, src: "https://www.googletagmanager.com/gtag/js?id=G-2V30EYKRT4" }],
+    ["script", null, "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-2V30EYKRT4');"]
   ],
+
+  // <!-- Global site tag (gtag.js) - Google Analytics -->
+  // <script async src="https://www.googletagmanager.com/gtag/js?id=G-2V30EYKRT4"></script>
+  // <script>
+  //   window.dataLayer = window.dataLayer || [];
+  //   function gtag(){dataLayer.push(arguments);}
+  //   gtag('js', new Date());
+  //   gtag('config', 'G-2V30EYKRT4');
+  // </script>
 
   /**
    * Theme configuration, here is the default theme configuration for VuePress.
